b1100bc562654dc87ddbb0ccd58b92afaac0c0a5
Brett C. Dudo <brett@dudo.io>
2020-04-25 21:47:08 -0700

    remove original service reference

--- lib/my_service/server.rb
+++ lib/my_service/server.rb
@@ -1,10 +1,7 @@
-require_relative '../card_service_pb'
-require_relative '../card_service_services_pb'
+require_relative '../my_service_pb'
+require_relative '../my_service_services_pb'
 
-module CardService
-  class Server < CardService::Service
-    def fetch_cards_from_set(set, _call)
-      PlayingCard.from_set(set).each { |card| Card.new(card) }
-    end
+module MyService
+  class Server < MyService::Service
   end
 end
